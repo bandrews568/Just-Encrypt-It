b91bfc16d6b1ea1c6840b0c580a624e7bfe15c06
Brandon <bandrews568@gmail.com>
2016-06-23 21:55:19 -0400

    Unit Tests

---
+++ tests.py
@@ -0,0 +1,71 @@
+import unittest
+from codecs import encode, decode 
+
+from main import EncryptText
+
+class JustEncryptItTest(unittest.TestCase):
+
+	def setUp(self):
+		self.encrypt = EncryptText()
+		#encrypted word: "test"
+		#password:"test" 
+		self.encrypted_128bit_str = "KVpSnAQB2bUp2OW4EXXmwQ==" 
+		self.encrypted_rot13_str = "grfg"
+		self.no_args_string = "(Select Encryption Type)"
+
+	def test_encryptstring_rot13(self):
+		"""
+			ROT 13 encryption
+			elif key == 'ROT 13'
+		"""
+		rot13_test = self.encrypt.encryptstring("test", "ROT 13")
+		test_string = encode("test", "rot13")
+		self.assertEqual(rot13_test, test_string)
+
+	def test_encryptstring_128bit(self):
+		"""
+			128 Bit Encryption
+			elif key == '128 Bit' and len(pass_key) > 0:
+		"""
+		_128bit_test = self.encrypt.encryptstring("test", 
+											"128 Bit", 
+											pass_key = "test")
+		self.assertEqual(_128bit_test, self.encrypted_128bit_str)
+
+	def test_encryptstring_no_args(self):
+		"""
+			*Else statement*
+			No arguments passed
+		"""
+		no_args_test = self.encrypt.encryptstring(None, None, None)
+		self.assertEqual(no_args_test, self.no_args_string)
+
+	def test_decryptstring_rot13(self):
+		"""
+			ROT 13 decryption
+			elif key == 'ROT 13'
+		"""
+		rot13_test = self.encrypt.decryptstring(self.encrypted_rot13_str, 
+											"ROT 13")
+		self.assertEqual(rot13_test, "test")
+
+	def test_decryptstring_128bit(self):
+		"""
+			128 Bit decryption
+			elif key == '128 Bit' and len(pass_key) > 0:
+		"""
+		_128bit_test = self.encrypt.decryptstring(self.encrypted_128bit_str, 
+											"128 Bit", 
+											pass_key = "test")
+		self.assertEqual(_128bit_test, "test")
+
+	def test_encryptstring_no_args(self):
+		"""
+			*Else statement*
+			No arguments passed
+		"""
+		no_args_test = self.encrypt.decryptstring(None, None, None)
+		self.assertEqual(no_args_test, self.no_args_string)
+		
+if __name__ == '__main__':
+    unittest.main()
